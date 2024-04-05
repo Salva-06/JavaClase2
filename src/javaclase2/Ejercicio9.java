3506cf6725b342083d64406a78ba5b888d79dd39
Salva <Salva@DESKTOP-E83V8PB>
2024-04-05 00:23:14 -0500

    ClaseN°2-EjercicioN°9

---
+++ src/javaclase2/Ejercicio9.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaclase2;
+import java.util.Scanner;
+/**
+ *
+ * @author Salva
+ */
+public class Ejercicio9 {
+public static void main(String[] arg){
+int n;
+Scanner sc = new Scanner (System.in);
+    System.out.println("Ingrese un numero: ");
+    n = sc.nextInt();
+    if (n%2==1){
+        System.out.println("El numero es impar");
+    }
+    if (n%2==0){
+        System.out.println("El numero es par");
+    }
+}
+}
